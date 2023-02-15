24b1e5380e1cf6531c5ed9de020811efd1a17dd7
piojank <p.jankowski931026@gmail.com>
2023-02-15 22:52:33 +0100

    Task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,10 @@
 const queryAll = (selector) => document.querySelectorAll(selector);
 
+const categories = queryAll("li.item");
 
+console.log(`Number of categories: ${categories.length}`);
+
+const message = categories.forEach((category) => {
+    console.log(`Category: ${category.children[0].textContent}`);
+    console.log(`Elements: ${category.children[1].children.length}`);
+});
