1834cff99c164b4160bac8f003d425a934329b53
piojank <p.jankowski931026@gmail.com>
2023-02-21 00:12:42 +0100

    Task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
-function getRandomHexColor() {
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const query = (selector) => document.querySelector(selector);
+
+const colorName= query(".color");
+const btnColor = query(".change-color");
+
+const changeColor = () => {
+  colorName.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+btnColor.addEventListener("click", changeColor);
