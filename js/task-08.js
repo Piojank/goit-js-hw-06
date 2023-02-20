256a44c3a5ae01b22c3fc0d8cf707d65cbb902b6
piojank <p.jankowski931026@gmail.com>
2023-02-21 00:00:21 +0100

    Task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const query = (selector) => document.querySelector(selector);
 
+const form = query(".login-form");
+
+const handleLogin = (event) => {
+    event.preventDefault();
+    const { 
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    alert("Successful login. Welcome!");
+    event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleLogin);
